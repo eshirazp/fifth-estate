96d5f0167b3125c3dfcd1c1500dd28fddda46bc0
Elush Shirazpour <eshirazp@gmail.com>
2017-07-29 13:15:27 -0700

    Additional code cleanup

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 
 const {connectDB, gracefulShutdown} = require('./models/connectDB');
-const executiveRouter = require('./routers/executiveRouter');
 const legislatorRouter = require('./routers/legislatorRouter');
 
 const {DATABASE_URL, PORT} = require('./config');
@@ -17,7 +16,6 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/views/index.html');
 });
-app.use('/prez', executiveRouter);
 app.use('/legs', legislatorRouter);
 
 let server;
